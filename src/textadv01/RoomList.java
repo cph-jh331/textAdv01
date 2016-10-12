5ac3b478481439d09767c54fd1dd417e40693701
bloch <bloch@10.50.131.58>
2016-10-12 09:59:50 +0200

    added name, diverse sout tekst i Text og ændrede nogle af room descriptions...

--- src/textadv01/RoomList.java
+++ src/textadv01/RoomList.java
@@ -43,36 +43,55 @@ public class RoomList {
                 + "", 5, 1, 4, 6, rand.randomGold());
         getRoomList().add(r);
         //rum 4
-        r = new Room(" found a somewhat lit room\n"
+        r = new Room(" walks slowly into a room, where light\n"
+                + "emanates from a small horse statue.\n"
                 + ""
                 + "", -1, 2, -1, 3, rand.randomGold());
         getRoomList().add(r);
         //rum 5
-        r = new Room(" entered a dead end!\n"
+        r = new Room(" goes into an alcove with a small rusty chest.\n"
+                + "The light from the previous room plays with the rust,\n"
+                + "making the chest seem alive.\n"
                 + ""
                 + "", -1, 3, -1, -1, rand.randomGold());
         getRoomList().add(r);
         //rum 6
-        r = new Room(" have entered a small room\n"
+        r = new Room(" enters a tiny room with axes, swords and a mural\n"
+                + "of a fearsome goat on the wall. The goat is covered\n"
+                + "in blood and seems incredibly evil.\n"
                 + ""
                 + "", -1, -1, 3, 7, rand.randomGold());
         getRoomList().add(r);
         //rum 7
-        r = new Room(" have entered a large room\n"
+        r = new Room(" walks into a large room filled with mixture of\n"
+                + "murals and statues of all kinds of farm animals.\n"
+                + "All of the creates does seem to like the presence\n"
+                + " of an uninvited guest.\n"
                 + "", 8, 9, 6, -1, rand.randomGold());
         getRoomList().add(r);
         //rum 8
-        r = new Room(" have entered a dead end!\n"
+        r = new Room(" walks into an alcove with a statue of\n"
+                + "a bloodthirsty bat. The bat seems to\n"
+                + "look at the uninvited guest with a deep\n"
+                + "hunger.\n"
                 + ""
                 + "", -1, 7, -1, -1, rand.randomGold());
         getRoomList().add(r);
         //rum 9
-        r = new Room(" have entered a large and light room.\n"
+        r = new Room(" enters a massive room made up by black polished marble.\n"
+                + "The room is filled with murals of bloodthirsty\n"
+                + "farm animals, even bunnies. Light emanates from\n"
+                + "a small whole at the top of the room.\n"
                 + "", 7, 10, -1, -1, rand.randomGold());
         getRoomList().add(r);
         //rum 10
         r = new Room(""
-                + " have reached the end of the Dungeon.\n"
+                + " enters a room with a massive black throne, on it sits a\n"
+                + "creature in full plate. The creature almost seems alive.\n"
+                + "\n"
+                + "The tunnel leading to the room suddenly collapses and\n"
+                + " darkness fills the room.\n"
+                + "This is the end…"
                 + "", -1, -1, -1, -1, rand.randomGold());
         getRoomList().add(r);
 
